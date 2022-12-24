87e549664c5ca023735e8c107fd6d28c46c20327
Joe <joelee8883@gmail.com>
2022-12-24 23:22:23 +0000

    Attempt at more graphical working indicator - does not work

--- code/processIndicator/main.cpp
+++ code/processIndicator/main.cpp
@@ -7,22 +7,38 @@
 
 #include <iostream>
 
+// More graphical text based progress bar
+void process(int scanLines, int renderedLines) {
+    int percentage = renderedLines/scanLines;
+    int position = percentage*50;
+    int remaining = 50-position;
+    printf("\r%3d%% [%.*s%*s]",
+           percentage,
+           position,
+           "||||||||||||||||||||||||||||||||||||||||||||||||||",
+           remaining,
+           "");
+    
+    fflush(stdout);
+}
+ 
 int main() {
 
-    // Image - increased dimensions for clarity and to show how the process indicator works 
+    // Image - increased dimensions for clarity and to show how the process indicator works
     const int image_width = 720;
     const int image_height = 720;
 
     // Render
-
     std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
     
     // Iterate each pixel
     for (int j = image_height-1; j >= 0; --j) {
         for (int i = 0; i < image_width; ++i) {
             
+            // Process indicator
             // std::flush - immediately output in each loop instead of outputting simultaneously
-            std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
+            //std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
+            process(j, image_width);
             
             // Change slightly colour in each loop
             auto r = double(i) / (image_width-1);
